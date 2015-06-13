76f27431c9c1808f0f233773a4c37ce1e0e63aff
Elisha Lai <elisha.lai@outlook.com>
2015-06-13 00:55:00 -0400

    Minor changes.

---
+++ Source-Code/Main/main.rb
@@ -0,0 +1,20 @@
+#==============================================================================
+# Watts
+#
+# @description: Program for performing Physics formula calculations
+# @author: Elisha Lai
+# @version: 0.0.1 15/06/2015
+#==============================================================================
+
+# Main program (main.rb)
+
+# Installs the required Ruby gem
+Shoes.setup do 
+  gem 'joules'
+end
+
+require 'joules'
+
+Shoes.app do
+  nil
+end
