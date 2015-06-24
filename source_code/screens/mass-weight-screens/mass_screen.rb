04f5fb252c12f7867ebf725a666c052eb1fe2acf
Elisha Lai <elisha.lai@outlook.com>
2015-06-23 23:21:36 -0400

    Minor changes.

--- source_code/screens/mass-weight-screens/mass_screen.rb
+++ source_code/screens/mass-weight-screens/mass_screen.rb
@@ -1,44 +1,44 @@
 #==============================================================================
 # Watts
 #
-# @description: Module for providing functions to work with WeightScreen
+# @description: Module for providing functions to work with MassScreen
 #   objects
 # @author: Elisha Lai
 # @version: 0.0.1 15/06/2015
 #==============================================================================
 
-# Weight screen module (weight_screen.rb)
+# Mass screen module (mass_screen.rb)
 
 require_relative '../../elements/screen_header.rb'
 require_relative '../../elements/screen_label.rb'
 require_relative '../../elements/screen_edit_line.rb'
 
 # Object definition
-class WeightScreen < Shoes
+class MassScreen < Shoes
 
-  url('/title_screen/mass_weight_screen/weight_screen',
-      :weight_screen)
+  url('/title_screen/mass_weight_screen/mass_screen',
+      :mass_screen)
 
-  # Draws the weight screen on the Shoes app window.
-  def weight_screen
-    @heading = 'Weight = mass × acceleration of free fall'
+  # Draws the mass screen on the Shoes app window.
+  def mass_screen
+    @heading = 'Mass = weight / acceleration of free fall'
     background('../images/mass_weight_large.png')
 
-    # Weight screen header
+    # Mass screen header
     ScreenHeader.new(self, '/title_screen/mass_weight_screen', @@font, @heading)
 
-    # Weight screen content
+    # Mass screen content
     flow(:height => 640, :width => 1080, :scroll => true) do
       # Left margin offset
       stack(:height => 640, :width => 80) do
       end
       # Content column 
       stack(:height => 640, :width => 1000) do
-        ScreenLabel.new(self, @@font, @heading, 'Mass')
+        ScreenLabel.new(self, @@font, @heading, 'Weight')
         flow do
-          @mass = ScreenEditLine.new(self, @@font, @heading)
-          @mass_unit = para(strong(' kg'))
-          @mass_unit.style(@@screen_unit_text_styles)
+          @weight = ScreenEditLine.new(self, @@font, @heading)
+          @weight_unit = para(strong(' N'))
+          @weight_unit.style(@@screen_unit_text_styles)
         end
 
         @calculate = button('Calculate')
@@ -49,11 +49,11 @@ class WeightScreen < Shoes
         
         @calculate.click do
           @result_display.clear do
-            @result = Joules.weight(@mass.text.to_f)
-            @weight = para(@result.to_s)
-            @weight_unit = para(' N')
-            @weight.style(@@screen_result_text_styles)
-            @weight_unit.style(@@screen_result_text_styles)
+            @result = Joules.mass(@weight.text.to_f)
+            @mass = para(@result.to_s)
+            @mass_unit = para(' kg')
+            @mass.style(@@screen_result_text_styles)
+            @mass_unit.style(@@screen_result_text_styles)
           end
         end
       end
