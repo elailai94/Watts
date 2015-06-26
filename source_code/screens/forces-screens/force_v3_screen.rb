b9f7714a0eb0159ac9137e18029981e17e1761dd
Elisha Lai <elisha.lai@outlook.com>
2015-06-26 00:45:46 -0400

    Minor changes.

--- source_code/screens/forces-screens/force_v3_screen.rb
+++ source_code/screens/forces-screens/force_v3_screen.rb
@@ -19,21 +19,35 @@ class ForceV3Screen < Shoes
   url('/title_screen/forces_screen/force_v3_screen',
       :force_v3_screen)
 
-  # Draws the force v1 screen on the Shoes app window.
-  def force_v1_screen
-    @heading = 'Force = mass x acceleration'
+  # Draws the force v3 screen on the Shoes app window.
+  def force_v3_screen
+    @heading = 'Force = ((final velocity - initial velocity) x mass) / time'
     background('../images/forces_large.png')
 
-    # Force v1 screen header
+    # Force v3 screen header
     ScreenHeader.new(self, '/title_screen/forces_screen', @@font, @heading)
 
-    # Force v1 screen content
+    # Force v3 screen content
     flow(:height => 640, :width => 1080, :scroll => true) do
       # Left margin offset
       stack(:height => 640, :width => 80) do
       end
       # Content column
       stack(:height => 640, :width => 1000) do
+        ScreenLabel.new(self, @@font, @heading, 'Initial velocity')
+        flow do
+          @initial_velocity = ScreenEditLine.new(self, @@font, @heading)
+          @initial_velocity_unit = para(strong(' ms'), strong(sup('-1')))
+          @initial_velocity_unit.style(@@screen_unit_text_styles)
+        end
+
+        ScreenLabel.new(self, @@font, @heading, 'Final velocity')
+        flow do
+          @final_velocity = ScreenEditLine.new(self, @@font, @heading)
+          @final_velocity_unit = para(strong(' ms'), strong(sup('-1')))
+          @final_velocity_unit.style(@@screen_unit_text_styles)
+        end
+
         ScreenLabel.new(self, @@font, @heading, 'Mass')
         flow do
           @mass = ScreenEditLine.new(self, @@font, @heading)
@@ -41,11 +55,11 @@ class ForceV3Screen < Shoes
           @mass_unit.style(@@screen_unit_text_styles)
         end
 
-        ScreenLabel.new(self, @@font, @heading, 'Acceleration')
+        ScreenLabel.new(self, @@font, @heading, 'Time')
         flow do
-          @acceleration = ScreenEditLine.new(self, @@font, @heading)
-          @acceleration_unit = para(strong(' ms'), strong(sup('-2')))
-          @acceleration_unit.style(@@screen_unit_text_styles)
+          @time = ScreenEditLine.new(self, @@font, @heading)
+          @time_unit = para(strong(' s'))
+          @time_unit.style(@@screen_unit_text_styles)
         end
 
         @calculate = button('Calculate')
@@ -56,12 +70,14 @@ class ForceV3Screen < Shoes
 
         @calculate.click do
           @result_display.clear do
-            @result = Joules.force_v1(@mass.text.to_f,
-                                      @acceleration.text.to_f)
-            @force_v1 = para(@result.to_s)
-            @force_v1_unit = para(' N')
-            @force_v1.style(@@screen_result_text_styles)
-            @force_v1_unit.style(@@screen_result_text_styles)
+            @result = Joules.force_v3(@initial_velocity.text.to_f,
+                                      @final_velocity.text.to_f,
+                                      @mass.text.to_f,
+                                      @time.text.to_f)
+            @force_v3 = para(@result.to_s)
+            @force_v3_unit = para(' N')
+            @force_v3.style(@@screen_result_text_styles)
+            @force_v3_unit.style(@@screen_result_text_styles)
           end
         end
       end
