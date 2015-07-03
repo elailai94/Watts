c36774204bf1a760ee988d0240302d09065b2076
Elisha Lai <elisha.lai@outlook.com>
2015-07-02 21:57:02 -0400

    Minor changes.

--- source-code/screens/oscillations_screen.rb
+++ source-code/screens/oscillations_screen.rb
@@ -32,10 +32,10 @@ class OscillationsScreen < Shoes
     # Oscillations screen content
     flow(:height => 640, :width => 1080, :scroll => true) do
       # Left margin offset
-      stack(:height => 640, :width => 80) do
+      stack(:height => 750, :width => 80) do
       end
       # Content column
-      stack(:height => 640, :width => 1000) do
+      stack(:height => 750, :width => 1000) do
         @formula_set =
           {'Particle displacement' => 'Particle displacement = amplitude x cosine(angular velocity x time)',
            'Maximum particle speed' => 'Maximum particle speed = angular velocity x amplitude'}
@@ -50,6 +50,45 @@ class OscillationsScreen < Shoes
           @particle_acceleration.style(@@screen_box_text_styles)
           click do
             visit("/title_screen/#{@heading_edited}_screen/particle_acceleration_screen")
+          end
+        end
+
+        stack(:height => 100, :width => 940, :margin => 2) do
+          background(@@theme_colour)
+          @particle_acceleration = para('Particle velocity = angular velocity x (amplitude', sup('2'),
+                                        ' - particle displacement', sup('2'), ')', sup('0.5'))
+          @particle_acceleration.style(@@screen_box_text_styles)
+          click do
+            visit("/title_screen/#{@heading_edited}_screen/particle_velocity_screen")
+          end
+        end
+
+        stack(:height => 100, :width => 940, :margin => 2) do
+          background(@@theme_colour)
+          @maximum_particle_acceleration = para('Maximum particle acceleration = angular velocity',
+                                                sup('2'), ' x amplitude')
+          @maximum_particle_acceleration.style(@@screen_box_text_styles)
+          click do
+            visit("/title_screen/#{@heading_edited}_screen/maximum_particle_acceleration_screen")
+          end
+        end
+
+        stack(:height => 100, :width => 940, :margin => 2) do
+          background(@@theme_colour)
+          @time_period_v2 = para('Time period = 2π x (mass / spring constant)', sup('0.5'))
+          @time_period_v2.style(@@screen_box_text_styles)
+          click do
+            visit("/title_screen/#{@heading_edited}_screen/time_period_v2_screen")
+          end
+        end
+
+        stack(:height => 100, :width => 940, :margin => 2) do
+          background(@@theme_colour)
+          @time_period_v3 = para('Time period = 2π x (pendulum length / acceleration of free fall)',
+                                 sup('0.5'))
+          @time_period_v3.style(@@screen_box_text_styles)
+          click do
+            visit("/title_screen/#{@heading_edited}_screen/time_period_v3_screen")
           end
         end
       end
